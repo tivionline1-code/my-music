6c2633ce7496bba35c77fb0b7035a89f48f66d1d
tivionline1-code <274575556+tivionline1-code@users.noreply.github.com>
2026-07-01 14:11:55 +0000

    Auto update music library

--- library.js
+++ library.js
@@ -1,5 +1,13 @@
 window.MUSIC_LIBRARY = [
   {
+    "id": "music/Fantasy orchestral/Bên Hồ Sao Rơi.mp3",
+    "title": "Bên Hồ Sao Rơi",
+    "artist": "Suno",
+    "album": "Fantasy orchestral",
+    "path": "music/Fantasy orchestral/Bên Hồ Sao Rơi.mp3",
+    "cover": ""
+  },
+  {
     "id": "music/Nhạc Không Lời/Fantasy Waltz 161.mp3",
     "title": "Fantasy Waltz 161",
     "artist": "Suno",
